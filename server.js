28c18bb317cfc8ba1f3b59df08eb48b8539a0604
Ryan Witherspoon <ry.witherspoon@gmail.com>
2020-11-19 20:23:35 -0700

    Updated Mongo

--- server.js
+++ server.js
@@ -15,10 +15,14 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
+const opts = {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
-});
+};
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", opts);
 
 // routes
 app.use(require("./routes/api.js"));
